3d39db845c2bf2bafb0c05c4a0c1e6ddbce9a30c
T-J-Antonio <tantonio@frba.utn.edu.ar>
2023-12-12 23:10:45 -0300

    day 12 part 2 almost done

--- day12/day12_part2.rb
+++ day12/day12_part2.rb
@@ -31,21 +31,21 @@ def consumed_condition_numbers(prev, conditions, condition_numbers, consumed)
 end
 
 def subtract_consumptions(array, consumptions, prev)
-  if consumptions == 0
-    array
-  elsif consumptions > 1
+  if consumptions > 1
     if array[0] > 1
       array[0] = array[0] - 1
     else
       array = array[1..]
     end
     subtract_consumptions(array, consumptions - 1, prev)
-  else
+  elsif consumptions == 1
     if prev == '.' && array[0] == 1
       array = array[1..]
     else
       array[0] = array[0] - 1
     end
+    array
+  else
     array
   end
 end
@@ -65,42 +65,39 @@ end
 def calculate_arrangements(condition_numbers, iteration_number, prev, consumed)
   if iteration_number == 4
     if prev == '.'
-      puts [condition_numbers, consumed, prev, 4, $dot_final[[consumed, condition_numbers.sum]]].to_s
-      return $dot_final[[consumed, condition_numbers.sum]]
+      return $dot_final[[consumed, condition_numbers]]
     else
-      puts [condition_numbers, consumed, prev, 4, $hash_final[[consumed, condition_numbers.sum]]].to_s
-      return $hash_final[[consumed, condition_numbers.sum]]
+      return $hash_final[[consumed, condition_numbers]]
     end
   end
   sum = 0
   if prev == '.'
     $dot_dot.each do |consumptions, occurrences|
       if consumptions[0] == consumed
-        new_conditions = subtract_consumptions(condition_numbers, consumptions[1], '.')
+        new_conditions = condition_numbers - consumptions[1]#subtract_consumptions(condition_numbers, consumptions[1], '.')
         sum = sum + occurrences * calculate_arrangements(new_conditions, iteration_number + 1, '.', consumed + consumptions[1])
       end
     end
     $dot_hash.each do |consumptions, occurrences|
       if consumptions[0] == consumed
-        new_conditions = subtract_consumptions(condition_numbers, consumptions[1], '#')
+        new_conditions = condition_numbers - consumptions[1]#subtract_consumptions(condition_numbers, consumptions[1], '#')
         sum = sum + occurrences * calculate_arrangements(new_conditions, iteration_number + 1, '#', consumed + consumptions[1])
       end
     end
   else
     $hash_dot.each do |consumptions, occurrences|
       if consumptions[0] == consumed
-        new_conditions = subtract_consumptions(condition_numbers, consumptions[1], '.')
+        new_conditions = condition_numbers - consumptions[1]#subtract_consumptions(condition_numbers, consumptions[1], '.')
         sum = sum + occurrences * calculate_arrangements(new_conditions, iteration_number + 1, '.', consumed + consumptions[1])
       end
     end
     $hash_hash.each do |consumptions, occurrences|
       if consumptions[0] == consumed
-        new_conditions = subtract_consumptions(condition_numbers, consumptions[1], '#')
+        new_conditions = condition_numbers - consumptions[1]#subtract_consumptions(condition_numbers, consumptions[1], '#')
         sum = sum + occurrences * calculate_arrangements(new_conditions, iteration_number + 1, '#', consumed + consumptions[1])
       end
     end
   end
-  puts [condition_numbers, consumed, prev, iteration_number, sum].to_s
   sum
 end
 
@@ -111,20 +108,17 @@ def arrangements_splitting(conditions, condition_numbers)
   $dot_hash = make_map(conditions, condition_numbers, '.', '#')
   $hash_dot = make_map(conditions, condition_numbers, '#', '.')
   $hash_hash = make_map(conditions, condition_numbers, '#', '#')
-  puts $dot_final.to_s, $hash_final.to_s, $dot_dot.to_s, $dot_hash.to_s, $hash_dot.to_s, $hash_hash.to_s
-  calculate_arrangements(Array.new(5).fill(condition_numbers).flatten, 0, '.', 0)
+  calculate_arrangements(condition_numbers.sum * 5, 0, '.', 0)
 end
 
 t = Benchmark.realtime do
-  lines = File.read("sarasa.txt").split("\n")
+  lines = File.read("day12_input.txt").split("\n")
   total = lines.reduce 0 do |prev, line|
     conditions = line.split(" ")[0]
     condition_numbers = line.split(" ")[1].split(",").map &:to_i
     arr = arrangements_splitting(conditions, condition_numbers)
-    puts arr
     prev + arr
   end
   puts total
 end
-
 puts t
